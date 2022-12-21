08627637b4121219805379df7dfe4292a0ba7d0e
ikokpsr <92991443+ikokpsr@users.noreply.github.com>
2022-12-21 12:58:23 +0700

    hoho

---
+++ Parkir2/Parkir2/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Parkir2
+{
+    partial class ParkirFastDetect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.cboDevice = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.stopDetect = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.oTimer = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 53);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(462, 304);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Camera";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(18, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(427, 265);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Location = new System.Drawing.Point(492, 53);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(462, 304);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Result";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(18, 19);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(427, 265);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // cboDevice
+            // 
+            this.cboDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDevice.FormattingEnabled = true;
+            this.cboDevice.Location = new System.Drawing.Point(76, 30);
+            this.cboDevice.Name = "cboDevice";
+            this.cboDevice.Size = new System.Drawing.Size(293, 21);
+            this.cboDevice.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(18, 79);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(95, 34);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start Camera";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(249, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start Detect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(18, 35);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(108, 64);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.stopDetect);
+            this.groupBox3.Controls.Add(this.btnStop);
+            this.groupBox3.Controls.Add(this.cboDevice);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.btnStart);
+            this.groupBox3.Location = new System.Drawing.Point(12, 363);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(459, 131);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Action";
+            // 
+            // stopDetect
+            // 
+            this.stopDetect.Location = new System.Drawing.Point(350, 79);
+            this.stopDetect.Name = "stopDetect";
+            this.stopDetect.Size = new System.Drawing.Size(95, 34);
+            this.stopDetect.TabIndex = 6;
+            this.stopDetect.Text = "Stop Detect";
+            this.stopDetect.UseVisualStyleBackColor = true;
+            this.stopDetect.Click += new System.EventHandler(this.stopDetect_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(119, 79);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(95, 34);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop Camera";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.textBox2);
+            this.groupBox4.Controls.Add(this.textBox1);
+            this.groupBox4.Controls.Add(this.pictureBox3);
+            this.groupBox4.Location = new System.Drawing.Point(492, 363);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(462, 131);
+            this.groupBox4.TabIndex = 7;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Capture";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(307, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(138, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(147, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "00:00:00";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Surf And Fast Detection";
+            // 
+            // oTimer
+            // 
+            this.oTimer.Interval = 1000;
+            this.oTimer.Tick += new System.EventHandler(this.oTimer_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Parking Time";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(335, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Parking Charge";
+            // 
+            // ParkirFastDetect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 506);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ParkirFastDetect";
+            this.Text = "Smart Parking";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ParkirFastDetect_FormClosing);
+            this.Load += new System.EventHandler(this.ParkirFastDetect_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox cboDevice;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer oTimer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button stopDetect;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Parkir2/Parkir2/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.Features2D;
+using Emgu.CV.XFeatures2D;
+using Emgu.CV.CvEnum;
+using AForge.Imaging.Filters;
+using Accord.Imaging;
+using System.Collections.Generic;
+
+namespace Parkir2
+{
+    public partial class ParkirFastDetect : Form
+    {
+        public ParkirFastDetect()
+        {
+            InitializeComponent();
+        }
+        FilterInfoCollection filter;
+        VideoCaptureDevice device;
+        private bool setz;
+        private bool setH;
+        int h, m, s;
+
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("cars.xml");
+        private void ParkirFastDetect_Load(object sender, EventArgs e)
+        {
+            filter = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in filter)
+                cboDevice.Items.Add(device.Name);
+            cboDevice.SelectedIndex = 0;
+            device = new VideoCaptureDevice();
+            device = new VideoCaptureDevice(filter[cboDevice.SelectedIndex].MonikerString);
+            device.NewFrame += Device_NewFrame;
+
+            Timer oTimer = new Timer(); 
+            oTimer.Interval = (45* 60 * 1000);
+            oTimer.Tick += new EventHandler(oTimer_Tick);
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            device = new VideoCaptureDevice(filter[cboDevice.SelectedIndex].MonikerString);
+            device.NewFrame += Device_NewFrame;
+            device.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            device.SignalToStop();
+            device.NewFrame -= new NewFrameEventHandler(Device_NewFrame);
+            device = null;
+        }
+
+        private void Device_NewFrame(Object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bit = (Bitmap)eventArgs.Frame.Clone();
+            Image<Bgr, Byte> grayImage = bit.ToImage<Bgr, byte>();
+                using (Graphics graphics = Graphics.FromImage(bit))
+                {
+                    using (Pen pen = new Pen(Color.Red, 2))
+                    {
+                    graphics.DrawRectangle(pen, 1190, 740, 200, 250);
+                    graphics.DrawRectangle(pen, 1420, 740, 200, 250);
+                    }
+                }
+            pictureBox1.Image = bit;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string num = "123456789";
+            int len = num.Length;
+            string otp = string.Empty;
+            int otpdigit = 5;
+            string finaldigit;
+
+            int getindex;
+            for (int i = 0; i < otpdigit; i++)
+            {
+                do
+                {
+                    getindex = new Random().Next(0, len);
+                    finaldigit = num.ToCharArray()[getindex].ToString();
+                }
+                while (otp.IndexOf(finaldigit) != -1);
+                otp += finaldigit;
+            }
+
+            string filename = @"D:\capture\" + otp + ".jpg";
+            Crop filter = new Crop(new Rectangle(1190, 740, 200, 250));
+            Bitmap bitmap = new Bitmap(pictureBox1.Image);
+            Bitmap newImage = filter.Apply(bitmap);
+            System.Drawing.Imaging.ImageFormat imageFormat = null;
+            imageFormat = System.Drawing.Imaging.ImageFormat.Jpeg;
+            newImage.Save(filename);
+            pictureBox3.Image = newImage;
+            setz = true;
+            oTimer.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (setz == true)
+            {
+                using (Bitmap bitm = new Bitmap(pictureBox3.Image))
+                using (Bitmap bitm2 = new Bitmap(pictureBox1.Image))
+                {
+                    Image<Bgr, byte> img = bitm.ToImage<Bgr, byte>();
+                    Image<Bgr, byte> img2 = bitm2.ToImage<Bgr, byte>();
+
+                    var gray = img.Convert<Gray, byte>();
+                    var gray2 = img2.Convert<Gray, byte>();
+                    int k = 2;
+                    double uniquenessThreshold = 0.8;
+                    Mat mask = new Mat();
+                    Mat homography = null;
+                    VectorOfVectorOfDMatch matches = new VectorOfVectorOfDMatch();
+                    VectorOfKeyPoint modelKeyPoints = new VectorOfKeyPoint();
+                    VectorOfKeyPoint observedKeyPoints = new VectorOfKeyPoint();
+
+                    FastFeatureDetector fast = new FastFeatureDetector(10, true);
+                    Freak descriptor = new Freak();
+
+                    Mat modelDescriptors = new Mat();
+                    //detector.DetectAndCompute(gray, null, modelKeyPoints, modelDescriptors, false);
+                    //var keypoint1 = fast.Detect(gray);
+                    fast.DetectRaw(gray, modelKeyPoints, null);
+                    descriptor.Compute(gray, modelKeyPoints, modelDescriptors);
+
+                    Mat observedDescriptors = new Mat();
+                    //detector.DetectAndCompute(gray2, null, observedKeyPoints, observedDescriptors, false);
+                    //var keypoint2 = fast.Detect(gray2);
+                    fast.DetectRaw(gray2, observedKeyPoints, null);
+                    descriptor.Compute(gray2, observedKeyPoints, observedDescriptors);
+
+                    //Features2DToolbox.DrawKeypoints(img, new VectorOfKeyPoint(keypoint1), modelDescriptors, new Bgr(255, 255, 0));
+                    //Features2DToolbox.DrawKeypoints(img2, new VectorOfKeyPoint(keypoint2), observedDescriptors, new Bgr(255, 255, 0));
+
+                    using (BFMatcher matcher = new BFMatcher(DistanceType.L2))
+                    {
+                        matcher.Add(modelDescriptors);
+
+                        matcher.KnnMatch(observedDescriptors, matches, k, null);
+                        mask= new Mat(matches.Size, 1, DepthType.Cv8U, 1);
+                        mask.SetTo(new MCvScalar(255));
+                        Features2DToolbox.VoteForUniqueness(matches, uniquenessThreshold, mask);
+
+                        int nonZeroCount = CvInvoke.CountNonZero(mask);
+                        if (nonZeroCount >= 4)
+                        {
+                           nonZeroCount = Features2DToolbox.VoteForSizeAndOrientation(modelKeyPoints, observedKeyPoints, matches, mask,
+                                1.5, 20);
+                            if (nonZeroCount >= 4)
+                                homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(modelKeyPoints, observedKeyPoints, matches,
+                                    mask, 2);
+                        }
+                        Mat result = new Mat();
+                        Features2DToolbox.DrawMatches(gray, modelKeyPoints, img2, observedKeyPoints, matches, result,
+                             new MCvScalar(255, 255, 255), new MCvScalar(255, 255, 255), mask);
+                        
+                        if (homography != null)
+                        {
+                            Rectangle rect = new Rectangle(System.Drawing.Point.Empty, gray.Size);
+                            PointF[] pts = new PointF[]
+                            {
+                                new PointF(rect.Left, rect.Bottom),
+                                new PointF(rect.Right, rect.Bottom),
+                                new PointF(rect.Right, rect.Top),
+                                new PointF(rect.Left, rect.Top)};
+                            pts = CvInvoke.PerspectiveTransform(pts, homography);
+                            System.Drawing.Point[] points = Array.ConvertAll<PointF, System.Drawing.Point>(pts, System.Drawing.Point.Round);
+
+                            using(VectorOfPoint vp = new VectorOfPoint(points))
+                            {
+                                CvInvoke.Polylines(result, vp, true, new MCvScalar(255, 0, 0, 255), 5);
+                            }
+                        }
+                        pictureBox2.Image = result.ToBitmap();
+                    }
+                }
+            }
+            //if (setz == false)
+            //{
+                
+           // }
+            if (setH == true)
+            {
+                Int32.TryParse(textBox1.Text, out int h);
+                if (h <= 1671318060)
+                {
+                    textBox2.Text = "Rp. 2000";
+                }
+                else if (h <= 1671318120)
+                {
+                    textBox2.Text = "Rp.4.000";
+                }
+                else if (h <= 1671318180)
+                {
+                    textBox2.Text = "Rp.6.000";
+                }
+                else if (h <= 1671318240)
+                {
+                    textBox2.Text = "Rp.8.000";
+                }
+                else if (h <= 1671318300)
+                {
+                    textBox2.Text = "Rp.10.000";
+                }
+                else
+                {
+                    textBox2.Text = "Lebih Dari 5 Menit";
+                }
+            }
+        }
+
+        private void stopDetect_Click(object sender, EventArgs e)
+        {
+            oTimer.Stop();
+            setH = true;
+        }
+
+        private void ParkirFastDetect_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (device.IsRunning == true)
+            {
+                this.device.Stop();
+            }
+        }
+
+        private void oTimer_Tick(object sender, EventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                s += 1;
+                if (s == 60)
+                {
+                    s = 0;
+                    m += 1;
+                }
+                if (m == 60)
+                {
+                    m = 0;
+                    h += 1;
+                }
+                textBox1.Text = String.Format("{0}:{1}:{2}", h.ToString().PadLeft(2, '0'), m.ToString().PadLeft(2, '0'), s.ToString().PadLeft(2, '0'));
+            }));
+
+        }
+    }
+}
